c8033e25accb461c2fe01239efbc0b533b918017
adonbiju-niveus <adon.biju@niveussolutions.com>
2023-05-18 12:21:23 +0530

    Created a file using fs module and async/await.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// create a promoise to display a mesaage and cal the promise
 function displayMessage() {
   return new Promise((resolve, reject) => {
     console.log("Hello, world!");
@@ -13,3 +14,22 @@ displayMessage()
   .catch((error) => {
     console.error("Error:", error);
   });
+
+
+//Create a file using fs module and async/await.
+const fs = require('fs');
+
+async function createFile(fileName,content) {
+  try {
+    if(!fs.existsSync('newfile.txt')){
+    await fs.promises.writeFile(fileName,content);
+    console.log('File created!');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+let fileName='newfile.txt'
+let content='Hello, new file!'
+createFile(fileName,content);
